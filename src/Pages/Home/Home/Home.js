aefdc4f2da5480e0566e6ad8077d3edba9e623ea
hira480 <hiramannan8@gmail.com>
2022-04-17 04:58:30 +0600

    loded servives data and display single service

---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Servises from '../Servises/Servises';
+
+const Home = () => {
+    return (
+        <div>
+            <Servises></Servises>
+        </div>
+    );
+};
+
+export default Home;
