ae1e35e35f499a735aa78e58a808d3282a1c1b78
hira480 <hiramannan8@gmail.com>
2022-04-18 04:22:08 +0600

    google login system implemented

--- src/Pages/Login/SocialLogin/SocialLogin.js
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -3,8 +3,23 @@ import './SocialLogin.css';
 import google from '../../../images/social/google.png';
 import github from '../../../images/social/github.png';
 import facebook from '../../../images/social/facebook.png';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
+import { useNavigate } from 'react-router-dom';
 
 const SocialLogin = () => {
+
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const navigate = useNavigate();
+    let errorText;
+
+    if (error) {
+        errorText = <p className='text-danger'>Error: {error.message}</p>
+    }
+    if (user) {
+        navigate('/home');
+    }
+
     return (
         <div>
             <div className='or'>
@@ -12,8 +27,10 @@ const SocialLogin = () => {
                 <div className='mx-2'>Or</div>
                 <div className='line'><hr /></div>
             </div>
+            {errorText}
             <div>
                 <button
+                    onClick={() => signInWithGoogle()}
                     className='btn btn-light d-block mx-auto my-2 btn-width'>
                     <img height={32} src={google} alt="" />
                     <span className='px-2'>Google Sign In</span>
